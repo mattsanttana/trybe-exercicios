ebcc24992018e3c4bff0d72ec07587e62389df25
mattsanttana <mths.santana@gmail.com>
2023-01-30 01:40:13 -0300

    exercico 03 concluido

---
+++ fundamentos/secao-03-introducao-a-java-script/dia-01-java-script-primeiros-passos/exercicio03.js
@@ -0,0 +1,11 @@
+const a = 69;
+const b = 157;
+const c = 666;
+
+if (a > b && a > c) {
+    console.log('a é o maior número')
+} else if (b > a && b > c) {
+    console.log('b é o maior número')
+} else {
+    console.log('c é o maior número')
+}
