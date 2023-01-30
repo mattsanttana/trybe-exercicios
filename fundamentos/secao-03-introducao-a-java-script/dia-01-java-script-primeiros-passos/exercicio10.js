2db3d7c8d2a50a27117a41d059a0faaaa553731d
mattsanttana <mths.santana@gmail.com>
2023-01-30 11:35:51 -0300

    exercicio 10 concluido

---
+++ fundamentos/secao-03-introducao-a-java-script/dia-01-java-script-primeiros-passos/exercicio10.js
@@ -0,0 +1,10 @@
+const number1 = 24;
+const number2 = 69;
+const number3 = 666;
+
+let impar = false;
+
+if ((number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0)) {
+  impar = true;
+};
+console.log(impar);
