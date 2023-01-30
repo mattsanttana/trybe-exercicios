0ccbace8acfc1933e4d625bb5b5439bf98f53ea9
mattsanttana <mths.santana@gmail.com>
2023-01-30 11:24:29 -0300

    atividade 09 concluida

---
+++ fundamentos/secao-03-introducao-a-java-script/dia-01-java-script-primeiros-passos/exercicio09.js
@@ -0,0 +1,9 @@
+const number1 = 24;
+const number2 = 69;
+const number3 = 666;
+
+if ((number1 % 2 === 0) && (number2 % 2 === 0) && (number3 % 2 === 0)) {
+    console.log('false')
+} else {
+    console.log('true')
+};
